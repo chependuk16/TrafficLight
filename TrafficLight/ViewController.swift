e895e95e1a975d56e1c591caf87bf8a7e2255a75
chependuk16 <chependuk16@gmail.com>
2023-01-31 02:31:08 +0300

    Made corner radiuses for views and button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -15,11 +15,18 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
+    private let lightViewCornerRadius: CGFloat = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLightView.layer.cornerRadius = lightViewCornerRadius
+        yellowLightView.layer.cornerRadius = lightViewCornerRadius
+        greenLightView.layer.cornerRadius = lightViewCornerRadius
+        startButton.layer.cornerRadius = 10
     }
 
     @IBAction func startButtonDidTapped() {
+    
     }
     
 }
